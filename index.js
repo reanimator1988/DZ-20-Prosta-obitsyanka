12c86f0646bc4bd6b28433dea37052e59b6f2e34
DiMa <francev.dima@gmail.com>
2023-07-12 21:38:23 +0200

    dz

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(function() {
+      resolve('Готово')
+
+      reject("Error")
+    }, 2000);
+});
+
+promise
+.then((result) => {
+  console.log(result);
+})
+.catch((error) => {
+  console.log(error);
+})
